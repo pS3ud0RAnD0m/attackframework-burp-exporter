2d74f447cc3117164480fa43badea217f0a9a9f7
pS3ud0RAnD0m <pS3ud0RAnD0m@users.noreply.github.com>
2025-06-23 01:41:37 -0400

    Renamed BurpExporter entry class

--- src/main/java/ai/attackframework/vectors/sources/burp/BurpExporter.java
+++ src/main/java/ai/attackframework/vectors/sources/burp/BurpExporter.java
@@ -3,7 +3,7 @@ package ai.attackframework.vectors.sources.burp;
 import burp.api.montoya.BurpExtension;
 import burp.api.montoya.MontoyaApi;
 
-public class AttackVectorsBurp implements BurpExtension {
+public class BurpExporter implements BurpExtension {
     @Override
     public void initialize(MontoyaApi api) {
         // extension bootstrap code here
